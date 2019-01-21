54f572dfab2dbe611ae02afedfc2acc71cc97086
Dan Douston <concordion@mac.com>
2019-01-21 11:00:49 -0500

    figured out how to render on click

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,37 +1,38 @@
 import React from 'react'
-import { Modal, Button, Alert } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import AlertClass from './Alert'
 
 const profile = require('../../assets/profile.png')
 
 export default class HomePage extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { isToggled: false }
+        // Gotta remember to bind functions
+        this.handleClick = this.handleClick.bind(this)
+    }
     
     render() {
         return (
-            <div>
+            <div className="container">
+                {this.state.isToggled ? <AlertClass></AlertClass> : null}
                 <center>
-                    <br></br>
                     <img alt="" src={profile} />
-                    <br></br>
                     <h1>Welcome to the Home Page</h1>
                 </center>
-                <br></br>
-                <div className="container">
-                    <h2>This is where Dan says stuff on the internet.</h2>
-                    <p>I hope to write many things here.</p>
-                    <Button bsStyle="primary" onClick={this.click}>
-                        Test Me
-                    </Button>
-                </div>
+                <h2>This is where Dan says stuff on the internet.</h2>
+                <p>I hope to write many things here.</p>
+                <Button bsStyle="primary" onClick={this.handleClick}>
+                    Test Me
+                </Button>
             </div>
         )
     }
 
-    // I am currently trying to figure out how to render this alert.
-    click() {
-        return (
-            <Alert bsStyle="warning">
-                <strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.
-            </Alert>
-        )
+    handleClick(props) {
+        this.setState(state => ({
+            isToggled: !state.isToggled
+        }))
     }
 }
