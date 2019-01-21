a9242a4b86bf76423620464e8d81f4f7af69041b
Dan Douston <concordion@mac.com>
2019-01-20 21:27:13 -0500

    adding bootstrap CSS stylesheet and a react-bootstrap module

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import * as Bootstrap from 'react-bootstrap'
+
+const profile = require('../../assets/profile.png')
 
 export default class HomePage extends React.Component {
     
     render() {
         return (
             <div>
-                <center><h1>Welcome to the Home Page</h1></center>
-                <h2>This is where Dan says stuff on the internet.</h2>
+                <center>
+                    <br></br>
+                    <img alt="" src={profile} />
+                    <br></br>
+                    <h1>Welcome to the Home Page</h1>
+                </center>
+                <br></br>
+                <div className="container">
+                    <h2>This is where Dan says stuff on the internet.</h2>
+                    <p>I hope to write many things here.</p>
+                    <Bootstrap.Button bsStyle="primary" onClick={this.click}>
+                        Test Me
+                    </Bootstrap.Button>
+                </div>
             </div>
+        )
+    }
+
+    // I am currently trying to figure out how to render this alert.
+    click() {
+        return (
+            <Bootstrap.Alert bsStyle="warning">
+                <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+                good.
+            </Bootstrap.Alert>
         )
     }
 }
