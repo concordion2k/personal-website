1ab51a14d461a1c493a32019e929e7f73381b9f0
Dan Douston <concordion@mac.com>
2019-01-20 21:40:29 -0500

    Proper imports

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import * as Bootstrap from 'react-bootstrap'
+import { Modal, Button, Alert } from 'react-bootstrap'
 
 const profile = require('../../assets/profile.png')
 
@@ -18,9 +18,9 @@ export default class HomePage extends React.Component {
                 <div className="container">
                     <h2>This is where Dan says stuff on the internet.</h2>
                     <p>I hope to write many things here.</p>
-                    <Bootstrap.Button bsStyle="primary" onClick={this.click}>
+                    <Button bsStyle="primary" onClick={this.click}>
                         Test Me
-                    </Bootstrap.Button>
+                    </Button>
                 </div>
             </div>
         )
@@ -29,10 +29,9 @@ export default class HomePage extends React.Component {
     // I am currently trying to figure out how to render this alert.
     click() {
         return (
-            <Bootstrap.Alert bsStyle="warning">
-                <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
-                good.
-            </Bootstrap.Alert>
+            <Alert bsStyle="warning">
+                <strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.
+            </Alert>
         )
     }
 }
